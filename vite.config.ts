b2f250de2599ac8da01ddbd557f21828a94b6643
IRISalman <mr.salman.khosravi@gmail.com>
2025-12-06 00:33:27 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+    // بارگذاری متغیرهای محیطی
     const env = loadEnv(mode, '.', '');
+    
     return {
+      // این خط خیلی مهمه برای اینکه روی گیت‌هاب صفحه سیاه نشه
+      base: '/Pouya/',
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
